38e16dc8cd4aab39cf097d9bfdf6818f22d8588a
Alex Burns <jharopa@gmail.com>
2025-02-25 14:05:29 +0000

    Headers test

--- pages/test.js
+++ pages/test.js
@@ -1,16 +1,5 @@
-import { headers } from 'next/headers';
+import { headers } from '../components/headers';
 
 export default async function LocalPage() {
-  const country = headers().get('wpe-headless-country') || 'No country data';
-  const region = headers().get('wpe-headless-region') || 'No region data';
-  const timezone = headers().get('wpe-headless-timezone') || 'No timezone data';
-
-  return (
-    <div>
-      <h1>Geolocation Data</h1>
-      <p>Country: {country}</p>
-      <p>Region: {region}</p>
-      <p>Timezone: {timezone}</p>
-    </div>
-  );
+  return <Headers />;
 }
